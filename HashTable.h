a84a41edcba00b621fc12a9ce7165689f55cab42
Wyatt Hooper <wyhoop@cs.csis.work>
2023-11-05 14:24:33 -0600

    Updating HashTable File

--- HashTable.h
+++ HashTable.h
@@ -12,44 +12,39 @@ using namespace std;
 template <typename DataType, typename KeyType>
 class HashTable {
 public:
-    HashTable(int initTableSize){
-	tablesize = initTablesize;
-	dataTable = new LinkedList<DataType, KeyType>* [tableSize];
+    HashTable(int initTableSize) {
+	tableSize = initTableSize;
+	dataTable = new LinkedList<DataType, KeyType>[tableSize];
     }
 
-    HashTable(const HashTable& other){
+    HashTable(const HashTable& other) {
 	copyTable(other);
     }
 
     HashTable& operator=(const HashTable& other) {
-	
-	//Cheak to make sure they aren't already the same table    
 	if(this != other) {
-		//Deallocate the memory before assigning new values
 		delete[] dataTable;
 
-		//Match this table size with the other
 		tableSize = other.tableSize;
 
-		//Dynamically allocate a new data table
 		dataTable = new LinkedList<DataType, KeyType>* [tableSize];
 
-		//Fill values in the new table
-		for(int i = 0; i < tableSize; i++) {
+		for(int i = o; i < tableSize; i++) {
 			dataTable[i] = other.dataTable[i];
 		}
 	}
-	//Return a reference to this object
-	return *this
+	return *this;
     }
 
     ~HashTable() {
 	    delete[] dataTable;
     }
 
-    void insert(const DataType& newDataItem, const KeyType& key) {
-	
+
+    void insert(const DataType& newDataItem, const KeyType& key){
+
     }
+
     bool remove(const KeyType& deleteKey);
     bool retrieve(const KeyType& searchKey, DataType& returnItem) const;
     void clear();
