66476025344ff2ff9e5c53cf2be62946609f1fbc
Wyatt Hooper <wyhoop@cs.csis.work>
2023-11-05 10:40:42 -0600

    Constructor through destructor in HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -12,13 +12,44 @@ using namespace std;
 template <typename DataType, typename KeyType>
 class HashTable {
 public:
-    HashTable(int initTableSize);
-    HashTable(const HashTable& other);
-    HashTable& operator=(const HashTable& other);
+    HashTable(int initTableSize){
+	tablesize = initTablesize;
+	dataTable = new LinkedList<DataType, KeyType>* [tableSize];
+    }
 
-    ~HashTable();
+    HashTable(const HashTable& other){
+	copyTable(other);
+    }
 
-    void insert(const DataType& newDataItem, const KeyType& key);
+    HashTable& operator=(const HashTable& other) {
+	
+	//Cheak to make sure they aren't already the same table    
+	if(this != other) {
+		//Deallocate the memory before assigning new values
+		delete[] dataTable;
+
+		//Match this table size with the other
+		tableSize = other.tableSize;
+
+		//Dynamically allocate a new data table
+		dataTable = new LinkedList<DataType, KeyType>* [tableSize];
+
+		//Fill values in the new table
+		for(int i = 0; i < tableSize; i++) {
+			dataTable[i] = other.dataTable[i];
+		}
+	}
+	//Return a reference to this object
+	return *this
+    }
+
+    ~HashTable() {
+	    delete[] dataTable;
+    }
+
+    void insert(const DataType& newDataItem, const KeyType& key) {
+	
+    }
     bool remove(const KeyType& deleteKey);
     bool retrieve(const KeyType& searchKey, DataType& returnItem) const;
     void clear();
